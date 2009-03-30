9792f722ef718181bfdfa8223c989a6a231815dd
Florian Ragwitz <rafl@debian.org>
2009-03-30 18:03:35 +0200

    Make it work.

--- lib/Audio/XMMSClient/AnyEvent.pm
+++ lib/Audio/XMMSClient/AnyEvent.pm
@@ -2,85 +2,60 @@ package Audio::XMMSClient::AnyEvent;
 use strict;
 use warnings;
 use AnyEvent;
+use IO::Handle;
 
 use base 'Audio::XMMSClient';
 
-sub _reopen {
-    my $id = shift;
-    open my $fh, ">&$id" or die "failed to dup $id";
-    return $fh;
-}
-
-sub _build_watcher {
-    my ($self, $name, $direction, $code) = @_;
-    if($self->{"has_${name}_watcher"}){
-        return $self->{"${name}_watcher"};
-    }
-    $self->{"${name}_watcher"} = AnyEvent->io(
-        fh   => _reopen($self->io_fd_get),
-        poll => $direction,
-        cb   => sub {
-            my $keep_watching = $self->$code;
-            if(!$keep_watching){
-                $self->_clear_watcher($name);
-            }
-        },
-    );
-    $self->{"has_${name}_watcher"} = 1;
-    return;
-}
-
-sub _clear_watcher {
-    my ($self, $name) = @_;
-    warn "deleting $name";
-    $self->{"has_${name}_watcher"} = 0;
-    delete $self->{"${name}_watcher"};
-    return;
-}
-
 sub connect {
     my $self = shift;
-    my $res = $self->SUPER::connect(@_);
+    my $ret = $self->SUPER::connect(@_);
+
+    $self->{fh} = IO::Handle->new_from_fd($self->io_fd_get, 'r+');
 
     $self->io_need_out_callback_set(\&need_out);
-    $self->_build_watcher('in', 'r', 'handle_in');
+    $self->{in_watch} = AnyEvent->io(
+        fh   => $self->{fh},
+        poll => 'w',
+        cb   => sub { $self->handle_in(@_) },
+    );
 
-    return $res;
+    return $ret;
 }
 
 sub need_out {
     my ($self, $flag) = @_;
 
-    if ($self->io_want_out && !$self->{has_out_watcher}) {
-        $self->_build_watcher('out', 'w', 'handle_out');
+    if ($self->io_want_out && !$self->{out_watch}) {
+        $self->{out_watch} = AnyEvent->io(
+            fh => $self->{fh},
+            poll => 'w',
+            cb => sub { $self->handle_out(@_) },
+        );
     }
 }
 
 sub handle_in {
     my ($self) = @_;
-    warn "handling input";
-    warn readline($self->io_fd_get);
     $self->io_in_handle;
     return 1;
 }
 
 sub handle_out {
     my ($self) = @_;
-    warn "io_out_handle";
     $self->io_out_handle;
-    return 1; # $self->io_want_out;
+    undef $self->{out_watch} unless $self->io_want_out;
 }
 
-sub get_loop {
-    warn "use the real event loop to do this";
-}
+{
+    my $quit = AnyEvent->condvar;
 
-sub loop {
-    warn "use the real event loop to do this";
-}
+    sub loop {
+        $quit->recv;
+    }
 
-sub quit_loop {
-    warn "use the real event loop to do this";
+    sub quit_loop {
+        $quit->send;
+    }
 }
 
 # sub request {
